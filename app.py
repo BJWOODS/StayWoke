2699a87c174922d47b3a3a45fcf399632efa5017
bwoodard <bwoodard@csumb.edu>
2017-04-01 06:43:41 +0000

    Mid push

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import flask
 import os
+import requests
 
 
 app = flask.Flask(__name__)
-
+__apikey__ = 'ab2396d1a7eb19b3bf5452a560714f0d' 
+cid = 'N00000019'
 @app.route('/')
 def hello():
-    return "Hello World"
     
+    apiKey = 'http://opensecrets.org/api/?method=candSummary&cid='+ cid + '&cycle=2012&apikey='+__apikey__
+    r = requests.get(apiKey).content
+    
+    return flask.render_template("index.html",apiKey = r,cid = cid) #passing in apiKey string to be used by index
+
+@app.route('/dez')
+def dez():
+    return flask.render_template('staywoke.html')
+
 app.run(
     port=int(os.getenv('PORT', 8080)),
     host=os.getenv('IP','0.0.0.0')
